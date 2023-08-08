fc5b86a557aa278dce74c01bef7c34ef35e0e3bf
PoolOfDeath20 <53143214+GervinFung@users.noreply.github.com>
2023-08-08 13:35:28 +0800

    Update prettier config (#15)
    * refactor(prettier): use `@poolofdeath20/prettier-config-generator` to generate `.prettierrc`
    
    * fix(cicd): point to proper tsconfig file for build to work
    
    * fix(lint): update `@poolofdeath20/eslint-config`

--- test/generate-package-json/index.test.ts
+++ test/generate-package-json/index.test.ts
@@ -3,19 +3,19 @@ import { describe, expect, it } from 'vitest';
 import generatePackageJson from '../../src/generate-package-json';
 
 describe('generate package.json for commonjs and module', () => {
-    it.each(['cjs', 'mjs'] as const)(
-        'should generate package.json for "%s"',
-        async (type) => {
-            expect(
-                await generatePackageJson({
-                    dir: path.join('test', 'dummy', 'happy-case'),
-                    outDir: type,
-                })
-            ).toBe(
-                `generated package.json for ${
-                    type === 'cjs' ? 'commonjs' : 'module'
-                }`
-            );
-        }
-    );
+	it.each(['cjs', 'mjs'] as const)(
+		'should generate package.json for "%s"',
+		async (type) => {
+			expect(
+				await generatePackageJson({
+					dir: path.join('test', 'dummy', 'happy-case'),
+					outDir: type,
+				})
+			).toBe(
+				`generated package.json for ${
+					type === 'cjs' ? 'commonjs' : 'module'
+				}`
+			);
+		}
+	);
 });
--- test/tally-transpiled-files/index.test.ts
+++ test/tally-transpiled-files/index.test.ts
@@ -2,32 +2,32 @@ import fs from 'fs';
 import path from 'path';
 import { describe, expect, it } from 'vitest';
 import {
-    tallyTranspiledFiles,
-    transpiledOutDirs,
+	tallyTranspiledFiles,
+	transpiledOutDirs,
 } from '../../src/tally-transpiled-files';
 
 describe('tally transpiled files', () => {
-    it('should make sure the transpiled directories are mjs and cjs', () => {
-        expect(transpiledOutDirs).toStrictEqual(['mjs', 'cjs']);
-    });
+	it('should make sure the transpiled directories are mjs and cjs', () => {
+		expect(transpiledOutDirs).toStrictEqual(['mjs', 'cjs']);
+	});
 
-    it('should tally transpiled JavaScript, Type Definition and Sourcemap files to make sure that all of the files exists in same folder', () => {
-        expect(
-            tallyTranspiledFiles(path.join('test', 'dummy', 'happy-case'))
-        ).toBe(true);
-    });
+	it('should tally transpiled JavaScript, Type Definition and Sourcemap files to make sure that all of the files exists in same folder', () => {
+		expect(
+			tallyTranspiledFiles(path.join('test', 'dummy', 'happy-case'))
+		).toBe(true);
+	});
 
-    it.each(
-        (() => {
-            const sadCase = path.join('test', 'dummy', 'sad-case');
-            return fs.readdirSync(sadCase).map((pathName) => {
-                return path.join(sadCase, pathName);
-            });
-        })()
-    )(
-        'should return false as all JavaScript, Type Definition and Sourcemap files must exists in the same folder',
-        (dir) => {
-            expect(tallyTranspiledFiles(dir)).toBe(false);
-        }
-    );
+	it.each(
+		(() => {
+			const sadCase = path.join('test', 'dummy', 'sad-case');
+			return fs.readdirSync(sadCase).map((pathName) => {
+				return path.join(sadCase, pathName);
+			});
+		})()
+	)(
+		'should return false as all JavaScript, Type Definition and Sourcemap files must exists in the same folder',
+		(dir) => {
+			expect(tallyTranspiledFiles(dir)).toBe(false);
+		}
+	);
 });
