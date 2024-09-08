09d6e2e0ed01c093eac728e8d57d67c63c6ccb57
PoolOfDeath20 <53143214+GervinFung@users.noreply.github.com>
2024-09-08 23:54:59 +0800

    Update package (#38)
    * chore(packages): update to latest non-breaking versions
    
    * chore(packages): update to latest breaking versions
    
    * lint(*): updated eslint from `v8` to `v9`

--- src/bin.ts
+++ src/bin.ts
@@ -1,3 +1,4 @@
 #!/usr/bin/env node
-import main from './';
-main();
+import main from '.';
+
+void main();
--- src/generate-package-json.ts
+++ src/generate-package-json.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import path from 'path';
+
 import { transpiledOutDirs } from './tally-transpiled-files';
 
 const generate = async (
