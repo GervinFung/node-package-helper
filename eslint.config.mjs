5b0b435615ccbed81f44d5d3cc93babbd8cd068d
PoolOfDeath20 <53143214+GervinFung@users.noreply.github.com>
2024-09-29 18:38:33 +0800

    Update package (#40)
    * chore(packages): update to latest non-breaking versions
    
    * chore(packages): update to latest breaking versions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,6 @@ import tseslint from 'typescript-eslint';
 export default tseslint.config(
 	includeIgnoreFile(`${process.cwd()}/.gitignore`),
 	eslint.configs.recommended,
-	...tseslint.configs.recommendedTypeChecked,
 	...tseslint.configs.strict,
 	...tseslint.configs.stylistic,
 	node,
